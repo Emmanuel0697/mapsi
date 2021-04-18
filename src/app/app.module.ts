b882cb00b80a8b69d8527bdd14fe384d442f1550
Emmanuell0697 <manuelreie@gmail.com>
2021-04-18 10:11:01 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GoogleMaps } from '@ionic-native/google-maps';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+ imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    GoogleMaps
+  ],bootstrap: [ AppComponent ]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { GoogleMaps, GoogleMapsEvent, LatLng, MarkerOptions, Marker } from "@ionic-native/google-maps";
+import { Platform } from "@ionic/angular";
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public platform: Platform) { }
 
-  constructor() {}
+	/* Only instantiate the map AFTER the view is initialized and the DOM is accessible */
+	ngAfterViewInit() {
+		this.platform.ready().then(() => this.loadMap());
+	}
 
+
+	loadMap() {
+		/* The create() function will take the ID of your map element */
+		const map = GoogleMaps.create('map');
+
+		map.one( GoogleMapsEvent.MAP_READY ).then((data: any) => {
+			const coordinates: LatLng = new LatLng(18.779241, -103.163511);
+
+			map.setCameraTarget(coordinates);
+			map.setCameraZoom(16);
+		});
+	}
 }
